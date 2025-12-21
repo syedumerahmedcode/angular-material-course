255f798ac41124ac55934227a7ef9af72f7f062a
umer <syedumerahmedcode@gmail.com>
2025-12-21 16:00:03 +0100

    Feature: Date picker

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 
 @Component({
@@ -22,6 +23,16 @@ export class CreateCourseStep1Component {
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
+
+  startDate=new Date(1900,1,1);
+
+  dateClass: MatCalendarCellClassFunction<Date>=(cellDate, view)=>{
+    const date=cellDate.getDate();
+    if(view=='month'){
+      return (date===1)?'highlight-date':'';
+    }
+    return '';
+  }
 
   constructor(private fb: UntypedFormBuilder) {
 
