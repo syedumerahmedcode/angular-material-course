18bff999bda50383c47bf204c8b206ee0dcdf780
umer <syedumerahmedcode@gmail.com>
2025-12-22 18:02:13 +0100

    Feature: Data table -multiple data selection.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -8,6 +8,7 @@ import {CoursesService} from "../services/courses.service";
 import {debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize} from 'rxjs/operators';
 import {merge, fromEvent, throwError} from "rxjs";
 import { Lesson } from '../model/lesson';
+import { SelectionModel } from '@angular/cdk/collections';
 
 
 @Component({
@@ -25,87 +26,17 @@ export class CourseComponent implements OnInit, AfterViewInit {
     @ViewChild(MatSort)
     sort:MatSort;
 
+    /**
+     * The following explaination is onyl for the parameters passed when initializing SelectionModel.
+     * </p>
+     * true: For allowing multiple selction from the data table (which is a list)
+     * [] : Represents the initial rows selected by the user.
+     */
+    selection = new SelectionModel<Lesson>(true, []);
 
 
-   /*  lessons = [
-       {
-        id: 120,
-        'description': 'Introduction to Angular Material',
-        'duration': '4:17',
-        'seqNo': 1,
-        courseId: 11
-      },
-      {
-        id: 121,
-        'description': 'Navigation and Containers',
-        'duration': '6:37',
-        'seqNo': 2,
-        courseId: 11
-      },
-      {
-        id: 122,
-        'description': 'Data Tables',
-        'duration': '8:03',
-        'seqNo': 3,
-        courseId: 11
-      },
-      {
-        id: 123,
-        'description': 'Dialogs and Overlays',
-        'duration': '11:46',
-        'seqNo': 4,
-        courseId: 11
-      },
-      {
-        id: 124,
-        'description': 'Commonly used Form Controls',
-        'duration': '7:17',
-        'seqNo': 5,
-        courseId: 11
-      },
-      {
-        id: 125,
-        'description': 'Drag and Drop',
-        'duration': '8:16',
-        'seqNo': 6,
-        courseId: 11
-      },
-      {
-        id: 126,
-        'description': 'Responsive Design',
-        'duration': '7:28',
-        'seqNo': 7,
-        courseId: 11
-      },
-      {
-        id: 127,
-        'description': 'Tree Component',
-        'duration': '11:09',
-        'seqNo': 8,
-        courseId: 11
-      },
-      {
-        id: 128,
-        'description': 'Virtual Scrolling',
-        'duration': '3:44',
-        'seqNo': 9,
-        courseId: 11
-      },
-      {
-        id: 129,
-        'description': 'Custom Themes',
-        'duration': '8:55',
-        'seqNo': 10,
-        courseId: 11
-      },
-      {
-        id: 130,
-        'description': 'Changing Theme at Runtime',
-        'duration': '12:37',
-        'seqNo': 11,
-        courseId: 11
-      }
-    ]; */
+
+   
 
     lessons:Lesson[] = [];
     constructor(private route: ActivatedRoute,
@@ -113,14 +44,17 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     }
 
-    displayedColumns = ['seqNo', 'description', 'duration'];
+    displayedColumns = ['select', 'seqNo', 'description', 'duration'];
     expandedLesson:Lesson=null;
 
     ngOnInit() {
-
       this.course = this.route.snapshot.data["course"];
       this.loadLessonsPage();
+    }
 
+    onLessonToggled(lesson:Lesson){
+      this.selection.toggle(lesson);
+      console.log(this.selection.selected);
     }
 
     /**
@@ -165,8 +99,18 @@ export class CourseComponent implements OnInit, AfterViewInit {
           tap(() => this.loadLessonsPage())
         )
         .subscribe();
+    }
 
+    isAllSelected() {
+      return this.selection.selected?.length == this.lessons?.length;
+    }
 
+    toggleAll(){
+      if(this.isAllSelected()){
+        this.selection.clear();
+      }else{
+        this.selection.select(...this.lessons);
+      }
     }
 
 }
