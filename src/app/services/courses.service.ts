7b7fa55cb90980af0d41122458bd738a905f2cfa
umer <syedumerahmedcode@gmail.com>
2025-12-22 11:29:54 +0100

    Feature: Added helpful comments in the Injectable CoursesService class for better understanding.

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,5 +1,11 @@
 
-
+/**
+ * @Injectable decorator: Marks the class as a service that can be injected into other components
+HttpClient: Used for making HTTP requests
+HttpParams: Helps create query parameters for API calls
+Observable: Handles asynchronous data streams
+map operator: Transforms the response data
+ */
 import {Injectable} from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import {Observable} from "rxjs";
@@ -15,17 +21,51 @@ export class CoursesService {
 
     }
 
+    /**
+     * Retrieves a single course by its ID
+     * Returns an Observable of a Course object
+     * Makes a GET request to /api/courses/{courseId}
+
+     * @param courseId The id of the course
+     * @returns an Observable of a Course object
+     */
     findCourseById(courseId: number): Observable<Course> {
         return this.http.get<Course>(`/api/courses/${courseId}`);
     }
 
+    /**
+     * Fetches all courses
+     * Uses the map operator to extract the 'payload' from the response
+     * Returns an Observable array of Course objects
+     * @returns 
+     */
     findAllCourses(): Observable<Course[]> {
         return this.http.get('/api/courses')
             .pipe(
                 map(res => res['payload'])
             );
+            /**
+             * pipe() Method
+                 Part of RxJS Observable chain
+                 Allows you to chain multiple operators together
+                 Used to transform, filter, or manipulate Observable streams
+               map() Operator
+                 Transforms each value emitted by an Observable
+                 Creates a new Observable with transformed values
+                 Works similarly to JavaScript's array .map() method
+             */
     }
 
+    /**
+     * Retrieves all lessons for a specific course
+    * Uses HttpParams to set query parameters:
+    * courseId: Identifies the course
+    * pageNumber: Set to 0
+    * pageSize: Set to 1000 (retrieves all lessons)
+    * Extracts 'payload' from the response
+     * @param courseId 
+     * @returns 
+     */
     findAllCourseLessons(courseId:number): Observable<Lesson[]> {
         return this.http.get('/api/lessons', {
             params: new HttpParams()
@@ -37,6 +77,27 @@ export class CoursesService {
         );
     }
 
+    /**
+     * Find a page of lessons of a given size for a given course
+     * 
+     * Most flexible method for fetching lessons
+     * Supports pagination and sorting
+     * Default parameters:
+     * sortOrder: Ascending
+     * pageNumber: 0
+     * pageSize: 3
+     * sortColumn: 'seqNo'
+     * Allows customization of lesson retrieval
+     * Please note: Uses RxJS map operator to transform responses
+     * 
+     * 
+     * @param courseId The courseId
+     * @param sortOrder How should we sort data?
+     * @param pageNumber The page number
+     * @param pageSize The size of the page
+     * @param sortColumn Sort data based on a particular column
+     * @returns 
+     */
     findLessons(
         courseId:number, sortOrder = 'asc',
         pageNumber = 0, pageSize = 3, sortColumn = 'seqNo'):  Observable<Lesson[]> {
