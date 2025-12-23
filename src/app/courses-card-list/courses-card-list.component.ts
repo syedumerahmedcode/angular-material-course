1cb4db6b2e1cda059bae821b7d1f4400b54f32ae
umer <syedumerahmedcode@gmail.com>
2025-12-23 11:18:16 +0100

    Feature: Responsive Breakpoint Observer Service.

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -3,6 +3,7 @@ import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 import { filter } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
     selector: 'courses-card-list',
@@ -14,12 +15,43 @@ export class CoursesCardListComponent implements OnInit {
 
     @Input()
     courses: Course[];
+    cols = 3;
+    rowHeight = '500px';
 
-    constructor(private dialog:MatDialog) {
+
+    /**
+     * Breakpoint observer service from angular material.
+     * It allows us to determine what form factor our screen is using.
+     */
+
+    constructor(private dialog:MatDialog,
+                private responsive:BreakpointObserver    ) {
     }
 
     ngOnInit() {
+        this.responsive.observe([
+            Breakpoints.TabletPortrait,
+            Breakpoints.TabletLandscape,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.HandsetLandscape
+        ])
+            .subscribe(result =>{
+                const breakpoints = result.breakpoints;
 
+                this.cols = 3;
+                this.rowHeight = "500px";
+
+                if(breakpoints[Breakpoints.TabletPortrait]){
+                    this.cols = 1;
+                } else if(breakpoints[Breakpoints.HandsetPortrait]){
+                    this.cols = 1;
+                    this.rowHeight= "430px";
+                } else if(breakpoints[Breakpoints.HandsetLandscape]){
+                    this.cols = 1;
+                } else if(breakpoints[Breakpoints.TabletLandscape]){
+                    this.cols = 2;
+                }
+            });
     }
 
     editCourse(course:Course) {
